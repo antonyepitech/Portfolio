20eb75f3486aac022d2c477c418ba14ac0d45bd3
anthony <anthony.cordon@epitech.eu>
2022-01-19 15:52:08 +0100

    full

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  postEmail: any;
+  postName: any;
+  postSubject: any;
+  postMessage: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private http: HttpClient) {
   }
 
+  // tslint:disable-next-line:typedef
+  ngOnInit() {}
+  // tslint:disable-next-line:typedef
+  clickbutton(e: Event){
+    const form: any = e.currentTarget;
+    const headers = {"Content-Type": "application/x-www-form-urlencoded"};
+    const body = {
+      email: form.elements.namedItem("email").value,
+      name: form.elements.namedItem("name").value,
+      subject: form.elements.namedItem("subject").value,
+      message: form.elements.namedItem("message").value,
+    };
+    console.log("toto", body);
+    this.http.post<any>('core/sendEmail.php', body, {headers})
+      .subscribe(data => {
+        console.log(data);
+        alert(data);
+      }, error => {
+        console.error(error);
+      });
+    e.preventDefault();
+  }
 }
+
--- src/app/home/strokes.js
+++ src/app/home/strokes.js
@@ -23,28 +23,113 @@ window.onload = function() {
 
   let banniere = document.getElementById('banniere');
 
+  let boxContact = document.getElementById('boxContact');
+  let boxSkills = document.getElementById('boxSkills');
+  let boxProject = document.getElementById('boxProject');
+  let boxAbout = document.getElementById('boxAbout');
+
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function () {
       buttonSelected = this;
       infoId.innerHTML = this.innerHTML;
-      infoId.style = "color: #2df5ff;";
-      infoId.style = "zIndex: -100;";
+      infoId.dataset.routeLink = this.dataset.title;
+      infoId.style.color = "#2df5ff;";
+      infoId.style.zIndex = "-100;";
     });
   }
-  infoId.addEventListener("mouseenter", function () {
-    let banniere = document.getElementById('banniere');
+
+  let box = document.querySelector(".box");
+
+  function play() {
+    box.className = "animBox";
+  }
+  function displayBox() {
     let paragraphe = document.getElementById("paragraphe");
-    if(infoId.innerHTML !== ""){
+    box.dataset.title = paragraphe.innerHTML;
+    console.log(box.dataset.title, paragraphe, paragraphe.innerHTML);
+    // if (paragraphe.innerHTML == "Projects") {
+    //   boxProject.style.display = "flex";
+    // } else if (paragraphe.innerHTML == "Skills") {
+    //   boxSkills.style.display = "flex";
+    // } else if (paragraphe.innerHTML == "Contact") {
+    //   boxContact.style.display = "flex";
+    // } else if (paragraphe.innerHTML == "About") {
+    //   boxContact.style.display = "flex";
+    // }
+  }
+
+  infoId.addEventListener("mouseenter", function () {
+
+    let paragraphe = document.getElementById("paragraphe");
+
+    if(infoId.innerHTML !== "" && infoId.innerHTML !== "Selected one subject") {
       banniere.style = "display: flex;"
+      infoId.addEventListener("click", play);
       for (let i = 0; i < buttons.length; i++) {
         paragraphe.innerHTML = buttonSelected.dataset.title;
+        displayBox();
       }
-      infoId.addEventListener("mouseleave", function () {
-        if (bool){
-          banniere.style = "display: none;"
-        }
-      })
+    }
+  });
+  infoId.addEventListener("mouseleave", function () {
+    if (bool){
+      banniere.style = "display: none;"
     }
   })
+
+
+  let arrow = document.querySelectorAll('.fas');
+  for(let i = 0; i < arrow.length; i++){
+    arrow[i].addEventListener("click", function (){
+      document.querySelector(".animBox").className = "backBox";
+      console.log("toto");
+    });
+  }
+
+
+  //PHONE//
+
+  let skillButton = document.querySelector(".skill");
+  let aboutMeButton = document.querySelector(".about");
+  let projectButton = document.querySelector(".project");
+  let contactMeButton = document.querySelector(".contactMe");
+
+
+  let phonesBox = document.getElementById("phone");
+  let skillsBox = document.getElementById("skillsBox");
+  let boxAboute = document.getElementById("boxAboute");
+  let projectBox = document.getElementById("boxProjects");
+  let boxContacts = document.getElementById("boxContacts");
+
+
+
+  skillButton.addEventListener("click", function(){
+    boxAboute.style.display = "none";
+    projectBox.style.display = "none";
+    boxContacts.style.display = "none";
+    skillsBox.style.display = "flex";
+  })
+
+  aboutMeButton.addEventListener("click", function(){
+    skillsBox.style.display = "none";
+    projectBox.style.display = "none";
+    boxContacts.style.display = "none";
+    boxAboute.style.display = "block";
+  })
+
+  projectButton.addEventListener("click", function(){
+    skillsBox.style.display = "none";
+    boxAboute.style.display = "none";
+    boxContacts.style.display = "none";
+    projectBox.style.display = "flex";
+  })
+
+  contactMeButton.addEventListener("click", function(){
+    skillsBox.style.display = "none";
+    boxAboute.style.display = "none";
+    projectBox.style.display = "none";
+    boxContacts.style.display = "flex";
+  })
+
 }
 
